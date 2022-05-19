316d2560becf8a3d4a44c22e2ab7a93817a7f086
tysepa <tysepa88@gmail.com>
2022-05-19 23:25:35 +0200

    ch(app): contact application
    It cover all course of react
    and crud operations
    and using Json server and axios to fecth
    
    working in progress

---
+++ src/componets/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+import AddContact from './AddContact'
+import ContactList from "./ContactList";
+
+function App() {
+  return (
+    <div >
+     <Header />
+     <><br></br><br></br><br></br><br></br></>
+     <AddContact />
+     <ContactList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componets/ContactList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactList = () => {
+  return (
+    <div>
+      <h2>Contact List</h2>
+    </div>
+  )
+}
+
+export default ContactList
